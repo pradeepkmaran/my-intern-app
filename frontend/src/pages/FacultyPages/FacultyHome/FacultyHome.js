27c54d31785a43b6183021628d82ffaf86fa2d1e
Pradeep KM <pradeepkm6122004@gmail.com>
2025-03-25 02:35:58 +0530

    feat: faculty home page styling

--- frontend/src/pages/FacultyPages/FacultyHome/FacultyHome.js
+++ frontend/src/pages/FacultyPages/FacultyHome/FacultyHome.js
@@ -1,25 +1,39 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../context/AuthContext";
+import './FacultyHome.css';
 
 const FacultyHome = () => {
     const { user, logout } = useContext(AuthContext);
     const navigate = useNavigate();
-  
+
     const handleViewClick = () => {
-      navigate("/faculty/view-internship-details");
+        navigate("/faculty/view-internship-details");
     };
-    
+
+    const handleLogout = () => {
+        logout();
+        navigate("/login");
+    };
+
     return (
-        <div>
-          <h1>Faculty Home</h1>
-          <h2>Welcome, {user ? user.email : "Guest"}!</h2>
-
-          <button onClick={handleViewClick}>
-            View Internship Details
-          </button> <br /> <br />
-
-          <button onClick={logout}>Logout</button>
+        <div className="faculty-home-container">
+            <h1>Faculty Home</h1>
+            
+            <h2>Welcome, {user ? user.email : "Guest"}!</h2>
+            
+            <div className="button-container">
+                <button onClick={handleViewClick}>
+                    View Internship Details
+                </button>
+                
+                <button 
+                    className="logout" 
+                    onClick={handleLogout}
+                >
+                    Logout
+                </button>
+            </div>
         </div>
     );
 }
